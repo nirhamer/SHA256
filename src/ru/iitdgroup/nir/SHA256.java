3f05d0d0657cf158375451eb40b4e57d7c6b7306
nir <nirhamer17@walla.co.il>
2020-04-15 16:23:12 +0300

    WIP SHA256 not sure why the TODO section answers weren't included this is a fix to that :)

--- src/ru/iitdgroup/nir/SHA256.java
+++ src/ru/iitdgroup/nir/SHA256.java
@@ -8,12 +8,26 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class SHA256 {
-
+    /**
+     *
+     * @param args when a user starts the program via command line he can give it arguments
+     * as text
+     * like
+     * javac SHA256.java
+     * java SHA256 hello world what up  as an argument
+     * in the code
+     * the args
+     * its an array containing all the user provided arguments
+     * this code would print out
+     * java Foo C://bla/foo/bar/data.txt
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         System.out.println(Arrays.toString(args));
 
 
-        //region Print command line arguments
+
         System.out.println("Argument count: " + args.length);
         for (int i = 0; i < args.length; i++) {
             System.out.println("Argument " + i + ": " + args[i]);
@@ -39,6 +53,11 @@ public class SHA256 {
         byte[] digestBytes = messageDigest.digest();
 
         //TODO: what is the difference between StringBuilder and StringBuffer
+        /*
+        StringBuffer is synchronized i.e. thread safe. It means two threads can't call the methods of StringBuffer simultaneously.
+        StringBuilder is non-synchronized i.e. not thread safe. It means two threads can call the methods of StringBuilder simultaneously.
+        therefore StringBuilder is more efficient than StringBuffer
+         */
         StringBuffer sb = new StringBuffer();
 
         for (int i = 0; i < digestBytes.length; i++) {
@@ -60,6 +79,19 @@ public class SHA256 {
         //TODO: what is the "magic" 0x100 ?
 
         //TODO: understand "&" operator https://www.baeldung.com/java-bitwise-operators
+        /*
+        Binary AND Operator copies a bit to the result if it exists in both operands.
+        Example (A & B) will give 12 which is 0000 1100
+
+
+        Bitwise operator works on bits and performs bit-by-bit operation. Assume if a = 60 and b = 13; now in binary format they will be as follows −
+
+a = 0011 1100
+
+b = 0000 1101
+
+a&b = 0000 1100
+        */
         return Integer.toString((digestByte & 0xff) + 0x100, 16).substring(1);
     }
 }
